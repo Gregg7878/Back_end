5aa78faa16b0bdb2489addb7f178cedefdc0f6d4
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-09-06 22:35:04 +0300

    modified users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    def create
+      @user = User.new(user_params)
+  
+      if @user.save
+       
+        render json: { message: 'User registered successfully' }, status: :created
+      else
+        
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :age, :password, :password_confirmation)
+    end
+  end
