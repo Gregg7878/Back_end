dc1949b0b7dc730e2a4827b743727a183ea1feda
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-09-06 23:43:01 +0300

    modified the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "authentication#index"
+
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
